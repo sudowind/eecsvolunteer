8789453ae0f6a1162adb311e15db6107cc89ccf1
Xian Wu <wuxian94@pku.edu.cn>
2016-04-19 13:11:12 +0800

    一些小bug的修复： 删除bootstrap_toolkit的import语句 调整js的导入顺序，在bootstrap之前导入jQuery

--- eecsvolunteer/logic/form.py
+++ eecsvolunteer/logic/form.py
@@ -1,7 +1,5 @@
 # coding=utf-8
 from django import forms
-from django.contrib.auth.models import User
-from bootstrap_toolkit.widgets import BootstrapDateInput, BootstrapTextInput, BootstrapUneditableInput
 
 
 class LoginForm(forms.Form):
