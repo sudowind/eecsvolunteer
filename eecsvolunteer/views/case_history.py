15b66a730e1511fdbacb050e8375c5797b238f82
Xian Wu <wuxian94@pku.edu.cn>
2016-04-20 20:26:24 +0800

    修改病历单排序

--- eecsvolunteer/views/case_history.py
+++ eecsvolunteer/views/case_history.py
@@ -10,6 +10,6 @@ from genius.models import CaseHistory
 
 def get_case_table(request):
     aid = request.GET.get('aid')
-    cases = CaseHistory.objects.filter(activity=aid).order_by('id')
+    cases = CaseHistory.objects.filter(activity=aid).order_by('status', 'id')
     data = qset2list(cases)
     return HttpResponse(json.dumps(data), content_type='application/json')
