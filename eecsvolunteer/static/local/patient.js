155d10899fdda43b32c418980907168f5edfd73a
wangfeng <wfpkueecs@163.com>
2016-04-22 20:53:05 +0800

    病历单提交之后不可再修改,下一步增加验证模块

--- eecsvolunteer/static/local/patient.js
+++ eecsvolunteer/static/local/patient.js
@@ -10,6 +10,8 @@ function add_case() {
         tips('请填写缺失项', 'info');
         return;
     }
+    var wait_count;
+    var id;
     var csrftoken = getCookie('csrftoken');
     $.ajax({
         url: 'add_case/',
@@ -26,11 +28,18 @@ function add_case() {
         success: function(data) {
             if (data['code'] == 1) {
                 tips('添加成功', 'success');
-                var wait_count = data['wait_count'];
-                var id = data['id']
-                $('#info_message').html(
-                    '<span style="color: green">流水号:' + id + ',还需等待' + wait_count.toString() + '人</span><br />'
-                );
+                wait_count = data['wait_count'];
+                id = data['id']
+                //$('#info_message').html(
+                //    '<span style="color: green">流水号:' + id + ',还需等待' + wait_count.toString() + '人</span><br />'
+                //);
+                $('#owner_info').html('<strong>机主:</strong>' + owner);
+                $('#contact_info').html('<strong>联系方式:</strong>' + contact);
+                $('#model_info').html('<strong>机型:</strong>' + model);
+                $('#problem_info').html('<strong>问题描述:</strong>' + problem);
+                $('#wait_info').html('<strong>等待人数:</strong>' + wait_count.toString());
+                $('#num_info').html('<strong>流水号:</strong>' + id.toString());
+                $('#create_case').hide();
             }
             else {
                 var msg = data['msg'] || '添加失败';
