c1cfafcc0138d7a44b31ef9044223ac32e4d97d2
sudowind <wfpkueecs@163.com>
2016-04-23 13:16:25 +0800

    从远程库删除db_settings.py

--- eecsvolunteer/db_settings.py
+++
@@ -1,9 +0,0 @@
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'eecs_test',
-        'USER': 'db_user',
-        'PASSWORD': '123456',
-        'HOST': '120.27.44.159',
-    }
-}
